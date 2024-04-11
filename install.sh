d76ad33f7bfc825260058353c9e289bf067eebb8
Roni Laukkarinen <roni@dude.fi>
2024-04-11 16:05:40 +0300

    Remove unused vars

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@ HOMEBREW_PATH='/opt/homebrew'
 
 # Helper variables
 txtbold=$(tput bold)
-boldyellow=${txtbold}$(tput setaf 3)
 boldgreen=${txtbold}$(tput setaf 2)
 yellow=$(tput setaf 3)
 green=$(tput setaf 2)
-white=$(tput setaf 7)
 txtreset=$(tput sgr0)
 
 echo "${yellow}Getting dependencies.${txtreset}"
@@ -34,40 +32,40 @@ sudo chmod -R 775 "${HOMEBREW_PATH}/etc/nginx/sites-available"
 sudo chmod -R 775 "${HOMEBREW_PATH}/etc/nginx/global"
 sudo echo "user $(whoami) staff;
 worker_processes 18;
-  
-events {  
+
+events {
         multi_accept on;
         accept_mutex on;
         worker_connections 1024;
 }
 
-http {  
+http {
 
-        ##  
-        # Optimization  
-        ##  
-  
+        ##
+        # Optimization
+        ##
+
         sendfile on;
         sendfile_max_chunk 512k;
-        tcp_nopush on;  
-        tcp_nodelay on;  
+        tcp_nopush on;
+        tcp_nodelay on;
         keepalive_timeout 120;
-        keepalive_requests 100000;  
+        keepalive_requests 100000;
         types_hash_max_size 2048;
         server_tokens off;
-        client_body_buffer_size      128k;  
-        client_max_body_size         10m;  
-        client_header_buffer_size    1k;  
-        large_client_header_buffers  4 32k;  
-        output_buffers               1 32k;  
+        client_body_buffer_size      128k;
+        client_max_body_size         10m;
+        client_header_buffer_size    1k;
+        large_client_header_buffers  4 32k;
+        output_buffers               1 32k;
         postpone_output              1460;
-  
-        server_names_hash_max_size 1024;  
-        #server_names_hash_bucket_size 64;  
-        # server_name_in_redirect off;  
-  
-        include ${HOMEBREW_PATH}/etc/nginx/mime.types;  
-        default_type application/octet-stream;  
+
+        server_names_hash_max_size 1024;
+        #server_names_hash_bucket_size 64;
+        # server_name_in_redirect off;
+
+        include ${HOMEBREW_PATH}/etc/nginx/mime.types;
+        default_type application/octet-stream;
 
         ##
         # Logging Settings
@@ -79,7 +77,7 @@ http {
         ##
         # Virtual Host Configs
         ##
-        
+
         include ${HOMEBREW_PATH}/etc/nginx/sites-enabled/*;
 }" > "${HOMEBREW_PATH}/etc/nginx/nginx.conf"
 sudo mkdir -p /var/log/nginx
